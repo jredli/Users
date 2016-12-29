df371fad665296916900e6ccaaf89e72e78871ed
Jovan Redli <some@nowhere.com>
2016-12-29 14:48:29 +0100

    strana all users

---
+++ Views/users/all_users.php
@@ -0,0 +1,12 @@
+<!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-2 col-lg-offset-5 text-center">
+                All users          
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
