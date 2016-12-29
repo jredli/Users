efcce79967464c9633c6f797e3072f832491cb74
Jovan Redli <some@nowhere.com>
2016-12-29 15:19:52 +0100

    Dodao select funkciju u Database

--- Libraries/Database.php
+++ Libraries/Database.php
@@ -8,4 +8,20 @@ class Database extends PDO{
 		//Povezivanje sa bazom
 		parent::__construct('mysql:host=localhost;dbname=users', 'root', '');;
 	}
+
+	//Osnovna funkcija za sve SELECT upite ka bazi
+	public function select($query, $bindValues = array(), $fetchMode = PDO::FETCH_ASSOC){
+		
+		//Prosleđivanje sql upita
+		$statement = $this->prepare($query);
+		//Postavljanje vrednosti koje se dinamički pune
+		foreach($bindValues as $key => $value){
+			$statement->bindValue("$key", $value);
+		}
+
+		$statement->execute();
+		return $statement->fetchAll($fetchMode);
+
+
+	}
 }
